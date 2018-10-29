6256c5122be6d054dbf6def6ec8c47130b2fdf18
Umar Mukhtar <umar@tuffah.info>
2018-10-29 17:52:17 +0800

    test 17

--- requestPage.php
+++ requestPage.php
@@ -1,6 +1,11 @@
 <html>
  <head>
    <link href="assets/images/favicon.png" rel="icon">
+   <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+   <script type='text/javascript' src="assets/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.min.css">
+   <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
  </head>
    <body>
      <div class="col-sm-12" style="margin-top: 2%; margin-bottom: 5%;">
@@ -102,12 +107,7 @@
 
      </div>
 
-     <script type="text/javascript" src="assets/js/jquery.min.js"></script>
-     <script type='text/javascript' src="assets/js/bootstrap.min.js"></script>
      <script type='text/javascript' src="assets/js/script.js"></script>
-     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.min.css">
-     <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
      <link rel="stylesheet" type="text/css" href="assets/css/style.css">
    </body>
 </html>
