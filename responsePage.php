10fdd476785e39f26d7827d8636900bb0f94e8b1
Umar Mukhtar <umar@tuffah.info>
2018-10-29 19:22:35 +0800

    test 27

--- responsePage.php
+++ responsePage.php
@@ -15,20 +15,20 @@
          <hr />
        </div>
        <div class="col-sm-3"><strong>Status</strong></div>
-       <div class="col-sm-9"><strong class="total"><?=$_POST['paymentStatus']; ?></strong></div>
+       <div class="col-sm-9"><strong class="total"><?=$data['paymentStatus']; ?></strong></div>
        <div class="col-sm-3"><strong>Billing Date</strong></div>
-       <div class="col-sm-9"><?=$_POST['billingDate']; ?></div>
+       <div class="col-sm-9"><?=$data['billingDate']; ?></div>
        <div class="col-sm-3"><strong>Bank Transaction No.</strong></div>
-       <div class="col-sm-9"><?=$_POST['bankReference']; ?></div>
+       <div class="col-sm-9"><?=$data['bankReference']; ?></div>
        <div class="col-sm-3"><strong>Reference No.</strong></div>
-       <div class="col-sm-9"><?=$_POST['reference']; ?></div>
+       <div class="col-sm-9"><?=$data['reference']; ?></div>
 
        <div class="col-sm-12">
          <hr />
          <h3>Thank you for your business.</h3>
 
          <p>The card with this number in
-           <strong><?=$_POST['credit-card-no']; ?></strong> has been successfully charged <strong><?=$_POST['currency']; ?> <?=$_POST['amount']; ?></strong> . A copy of this receipt is also in your transaction history.</p>
+           <strong><?=$data['credit-card-no']; ?></strong> has been successfully charged <strong><?=$data['currency']; ?> <?=$data['amount']; ?></strong> . A copy of this receipt is also in your transaction history.</p>
          <p>If you have any questions, please let us know. We'll get back to you as soon as we can.</p>
          <a href="!#">support@greenpacket.net</a>
        </div>
@@ -37,16 +37,16 @@
          <hr />
          <strong>Credit-Card Information</strong>
          <ul>
-           <li><?=$_POST['name']; ?></li>
-           <li><?=$_POST['credit-card-no']; ?></li>
-           <li><?=$_POST['banks']; ?></li>
+           <li><?=$data['name']; ?></li>
+           <li><?=$data['credit-card-no']; ?></li>
+           <li><?=$data['banks']; ?></li>
          </ul>
        </div>
 
        <div class="col-sm-12">
          <hr />
          <strong>Total :</strong>
-         <strong class="total"><?=$_POST['currency']; ?> <?=$_POST['amount']; ?></strong>
+         <strong class="total"><?=$data['currency']; ?> <?=$data['amount']; ?></strong>
        </div>
 
        <div class="col-sm-12">
