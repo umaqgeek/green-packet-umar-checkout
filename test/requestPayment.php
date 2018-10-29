1c1a15dd6ea0c6a0defbb9aa932b74b0219d9d8f
Umar Mukhtar <umar@tuffah.info>
2018-10-29 22:14:14 +0800

    add test

--- test/requestPayment.php
+++ test/requestPayment.php
@@ -1,10 +1,36 @@
 <?php
 use PHPUnit\Framework\TestCase;
+require('vendor/autoload.php');
 
-class RequestPayment extends TestCase {
-	public function testCalculationOfMax() {
-    $numbers = array(5, 2, 10, 11, 7, 9);
-    $expectedResult = 11;
-		$this->assertEquals($expectedResult, max($numbers));
+class RequestPayment extends PHPUnit_Framework_TestCase {
+
+	protected $client;
+
+  protected function setUp() {
+  	$this->client = new GuzzleHttp\Client([
+    	'base_uri' => 'https://green-packet-umar-checkout.herokuapp.com'
+    ]);
+  }
+
+	public function testTest01() {
+    $this->assertEquals('qwerty', 'qwerty');
 	}
+
+	public function testPost_InvalidInput_Amount() {
+  	$response = $this->client->request('POST', '/', [
+    	'form_params' => [
+      	'amount' => 252.39,
+				'merchantId' => 'f2e4aa62a1f79f1a43a1fcbb86e0ebf2',
+				'reference' => 'T001'
+      ]
+    ]);
+
+		print_r($response);
+
+    $this->assertEquals(200, $response->getStatusCode());
+
+    $data = json_decode($response->getBody(), true);
+
+    $this->assertEquals('Invalid amount', $data);
+  }
 }
