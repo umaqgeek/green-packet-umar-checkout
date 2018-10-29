554e6e2508a5bb92f85873be9e06c2a9e52dfd75
Umar Mukhtar <umar@tuffah.info>
2018-10-29 16:33:54 +0800

    test 13

--- func.php
+++ func.php
@@ -5,6 +5,7 @@ function isValidMerchant($merchantId) {
   $status = false;
 
   $sql = sprintf("SELECT m.m_id FROM merchants m WHERE MD5(CONCAT(m.m_id, m.m_salt)) = '$s'", $merchantId);
+  echo $sql;
 
   $result = $GLOBALS['conn']->query($sql);
 
