de87605a52f8fba19300de94a95894fecfd63597
Umar Mukhtar <umar@tuffah.info>
2018-10-29 19:19:10 +0800

    test 26

--- requestPage.php
+++ requestPage.php
@@ -16,12 +16,12 @@
            <span class=""></span> <strong>Summary of Transaction</strong>
            <hr />
          </div>
-         <div class="col-sm-2"><strong>Net Charges</strong></div>
-         <div class="col-sm-10">MYR <?=$data['amount']; ?></div>
-         <div class="col-sm-2"><strong>Pay To</strong></div>
-         <div class="col-sm-10"><?=$data['merchant']['m_email']; ?></div>
-         <div class="col-sm-2"><strong>Reference ID</strong></div>
-         <div class="col-sm-10"><?=$data['reference']; ?></div>
+         <div class="col-sm-3"><strong>Net Charges</strong></div>
+         <div class="col-sm-9">MYR <?=$data['amount']; ?></div>
+         <div class="col-sm-3"><strong>Pay To</strong></div>
+         <div class="col-sm-9"><?=$data['merchant']['m_email']; ?></div>
+         <div class="col-sm-3"><strong>Reference ID</strong></div>
+         <div class="col-sm-9"><?=$data['reference']; ?></div>
 
          <div class="col-sm-12"><hr /></div>
 
